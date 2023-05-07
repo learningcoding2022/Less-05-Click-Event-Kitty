48bd61cfe1db16e7f9b36928918b2287709f524e
molly_b <brownmolly109@gmail.com>
2023-05-07 15:54:35 -0400

    if-else-statements

--- js/script.js
+++ js/script.js
@@ -5,8 +5,16 @@ var cat = document.querySelector(".cat");
 /*console.log(catFigure);*/
 
 /* When user clicks on the button, function is triggered to show cat image*/
-button.addEventListener("click", function () {
-    button.innerText = "it's working.";
-    cat.classList.add("show");
-/*don't have to have the "." before a class when adding the class*/
+button.addEventListener("click", function () { 
+if (cat.classList.contains("show")) {
+  /*console.log("Yes.");*/
+   cat.classList.remove("show");
+   button.innerText = ("Wait, come back!");
+   button.classList.add("disappear");
+} else {
+    cat.classList.add("show");  
+    /*console.log("No.");*/
+    button.innerText = ("Shoo, cat!!");
+    button.classList.remove("disappear");
+}
 });
